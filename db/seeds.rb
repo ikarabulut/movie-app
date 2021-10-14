93c2cdda4dafd86066f0c6636094b271a0d9c962
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-13 20:00:22 -0500

    adds ffaker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
-actor.save
+# actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+# actor.save
 
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-actor.save
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
+
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
+
+# actor = Actor.new(first_name: "Matt", last_name: "Damon", known_for: "Bourne")
+# actor.save
+
+# actor = Actor.new(first_name: "Jennifer", last_name: "Aniston", known_for: "Friends")
+# actor.save
+
+# actor = Actor.new(first_name: "Megan", last_name: "Fox", known_for: "Transformers")
+# actor.save
