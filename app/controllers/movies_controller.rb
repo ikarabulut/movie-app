119a23ae2700916b5d858fbd922e5fea3487e85f
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-17 19:37:48 -0500

    adds show controller and route, and new post route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,16 @@ class MoviesController < ApplicationController
     render json: all_movies.as_json
   end
 
+  def show
+    index = params[:id]
+    actor = Actor.find_by(id: index)
+    render json: actor.as_json
+  end
+
+  def post_actor
+    index = params[:id]
+    actor = Actor.find_by(id: index)
+    render json: actor.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/all_movies" => "movies#all_movies"
+  get "/all_movies/actor" => "movies#show"
+  get "/all_movies/actor/:id" => "movies#show"
+  post "/all_movies/actor" => "movies#post_actor"
 end
