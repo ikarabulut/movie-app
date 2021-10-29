769a068317b4da55d3cf46d717eff0d09d268019
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-29 10:40:29 -0500

    adds associations and movie serializer

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,12 @@ class Movie < ApplicationRecord
   validates :plot, length: {in: 50..5000}
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    movies = Movie.all
+    genre_names = movies.genres.names
+    return genre_names
+  end
 end
